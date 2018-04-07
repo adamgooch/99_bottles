8d95fce88c861a007bd211132f5e6b987f89cf63
Adam Gooch <adam.gooch@accelerated.com>
2018-04-07 18:54:10 -0400

    minor refactors

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,18 +1,16 @@
 class Bottles
-  def verse(number_of_bottles)
-    if(number_of_bottles) == 0
-      return no_more_bottles
-    end
-    first_stanza(number_of_bottles) + final_stanza(number_of_bottles - 1)
+  def verse(bottle_count)
+    return no_more_bottles if(bottle_count) == 0
+    first_stanza(bottle_count) + second_stanza(bottle_count - 1)
   end
 
-  def verses(first_verse_bottles, last_verse_bottles)
-    current_bottles = first_verse_bottles
+  def verses(first_bottle_count, last_bottle_count)
+    current_bottles = first_bottle_count
     result = "#{verse(current_bottles)}\n"
-    while(current_bottles > last_verse_bottles)
+    while(current_bottles > last_bottle_count)
       current_bottles = current_bottles - 1
       result += "#{verse(current_bottles)}"
-      result += "\n" if current_bottles > last_verse_bottles
+      result += "\n" if current_bottles > last_bottle_count
     end
     result
   end
@@ -23,25 +21,26 @@ class Bottles
 
   private
 
-  def pluralize(count, word)
-    count > 1 ? "#{word}s" : word
+  def pluralize_bottle(count)
+    count > 1 ? "bottles" : "bottle"
   end
 
-  def first_stanza(number_of_bottles)
-    "#{number_of_bottles} #{pluralize(number_of_bottles, 'bottle')} " +
-    "of beer on the wall, " +
-    "#{number_of_bottles} #{pluralize(number_of_bottles, 'bottle')} " +
-    "of beer.\n"
+  def number_of_bottles(count)
+    "#{count} #{pluralize_bottle(count)}"
   end
 
-  def final_stanza(number_of_bottles)
-    if(number_of_bottles == 0)
+  def first_stanza(count)
+    "#{number_of_bottles(count)} of beer on the wall, " +
+    "#{number_of_bottles(count)} of beer.\n"
+  end
+
+  def second_stanza(count)
+    if(count > 0)
+      "Take one down and pass it around, " +
+      "#{number_of_bottles(count)} of beer on the wall.\n"
+    else
       "Take it down and pass it around, " +
       "no more bottles of beer on the wall.\n"
-    else
-      "Take one down and pass it around, " +
-      "#{number_of_bottles} #{pluralize(number_of_bottles, 'bottle')} " +
-      "of beer on the wall.\n"
     end
   end
 
